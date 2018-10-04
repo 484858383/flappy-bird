21b82a1a9a5bac1a8db144e48a25ef4ca4f10d55
Ronnie Kaczynski <42363811+484858383@users.noreply.github.com>
2018-10-04 18:55:43 +0100

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -1,29 +1,48 @@
 #include"Game.h"
 
 #include<SFML/System/Clock.hpp>
+#include<iostream>
 
 Game::Game()
 {
-    m_window.create(sf::VideoMode(1024,512),"window", sf::Style::Close);
+    m_window.create(sf::VideoMode(1024,512), "window", sf::Style::Close);
+    m_window.setFramerateLimit(120);
 }
 
 void Game::run()
 {
     float dt = 0.0f;
+    sf::Clock timer; //every second a new set pipes is added
+
     while(m_window.isOpen())
     {
-        sf::Clock c;
+        sf::Clock frameTimer;
 
         m_window.clear();
         handleInput(dt);
 
-        m_bird.update(dt);
+        for(auto& pipe : m_pipes)
+            pipe->draw(m_window);
+
         m_bird.draw(m_window);
+
+        m_bird.update(dt);
+
+        for(auto& pipe : m_pipes)
+            pipe->update(dt);
 
         m_window.display();
         handleEvents();
 
-        dt = c.getElapsedTime().asSeconds();
+        dt = frameTimer.getElapsedTime().asSeconds();
+
+        if(timer.getElapsedTime().asSeconds() >= 1.50f)
+        {
+            addPipe();
+            timer.restart();
+        }
+
+        deletePipes();
     }
 }
 
@@ -51,4 +70,20 @@ void Game::handleEvents()
     }
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
         m_window.close();
+}
+
+void Game::addPipe()
+{
+    m_pipes.push_back(std::make_unique<Pipe>());
+}
+
+void Game::deletePipes()
+{
+    for(auto iter = m_pipes.begin(); iter != m_pipes.end();)
+    {
+        if((*iter)->getPosition().x < -128.0f)
+            iter = m_pipes.erase(iter);
+        else
+            iter++;
+    }
 }
