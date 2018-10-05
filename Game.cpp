61c435a2486f101274931d56ea5130d501585d05
Ronnie Kaczynski <42363811+484858383@users.noreply.github.com>
2018-10-06 00:30:26 +0100

    changed main game loop
    created update and render functions for the ready/losing states

--- Game.cpp
+++ Game.cpp
@@ -4,6 +4,7 @@
 #include<iostream>
 
 Game::Game()
+:m_loss(false), m_ready(false)
 {
     m_window.create(sf::VideoMode(1024,512), "window", sf::Style::Close);
     m_window.setFramerateLimit(120);
@@ -12,8 +13,6 @@ Game::Game()
 void Game::run()
 {
     float dt = 0.0f;
-    sf::Clock timer; //every second a new set pipes is added
-
     while(m_window.isOpen())
     {
         sf::Clock frameTimer;
@@ -21,39 +20,54 @@ void Game::run()
         m_window.clear();
         handleInput(dt);
 
-        for(auto& pipe : m_pipes)
-            pipe->draw(m_window);
+        if(m_ready)
+            m_bird.update(dt); //out of update function so bird can fall once player loses
 
-        m_bird.draw(m_window);
+        if(!m_loss && m_ready)
+            update(dt);
 
-        m_bird.update(dt);
-
-        for(auto& pipe : m_pipes)
-            pipe->update(dt);
+        render();
 
         m_window.display();
         handleEvents();
 
         dt = frameTimer.getElapsedTime().asSeconds();
-
-        if(timer.getElapsedTime().asSeconds() >= 1.50f)
-        {
-            addPipe();
-            timer.restart();
-        }
-
-        deletePipes();
     }
 }
 
+void Game::update(float dt)
+{
+    static sf::Clock timer; //every second a new set pipes is added
+
+    for(auto& pipe : m_pipes)
+        pipe->update(dt);
+    handleCollisions();
+
+    if(timer.getElapsedTime().asSeconds() >= 1.50f)
+    {
+        addPipe();
+        timer.restart();
+    }
+    deletePipes(); //this should ideally be last operation on the pipes
+}
+
+void Game::render()
+{
+    for(auto& pipe : m_pipes)
+        pipe->draw(m_window);
+   m_bird.draw(m_window);
+}
+
+
 void Game::handleInput(float dt)
 {
-    sf::Event e;
-    while(m_window.pollEvent(e))
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::R) && m_loss)
+        reset();
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && !m_ready ||
+       sf::Mouse::isButtonPressed(sf::Mouse::Left) && !m_ready)
     {
-        if(e.type == sf::Event::MouseButtonPressed)
-        if(e.mouseButton.button == sf::Mouse::Left)
-            m_bird.bounce();
+        m_ready = true;
+        m_bird.bounce();
     }
 }
 
@@ -62,14 +76,22 @@ void Game::handleEvents()
     sf::Event e;
     while(m_window.pollEvent(e))
     {
-        if(e.type == sf::Event::Closed)
+        switch(e.type)
+        {
+        case sf::Event::Closed:
             m_window.close();
-        if(e.type == sf::Event::MouseButtonPressed)
-            if(e.mouseButton.button == sf::Mouse::Left)
+        case sf::Event::KeyPressed:
+            if(e.key.code == sf::Keyboard::Escape)
+                m_window.close();
+        case sf::Event::MouseButtonPressed:
+            if(e.mouseButton.button == sf::Mouse::Left && !m_loss)
                 m_bird.bounce();
+        default:
+            break;
+        }
     }
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-        m_window.close();
+    //sadly mouse input has to go here as I want to limit bounces to button clicks
+    //sf::IsButtonPressed() does not account for this and having two event loops doesnt work either
 }
 
 void Game::addPipe()
@@ -86,4 +108,29 @@ void Game::deletePipes()
         else
             iter++;
     }
+}
+
+void Game::handleCollisions()
+{
+    for(auto iter = m_pipes.begin(); iter != m_pipes.end(); iter++)
+    {
+        if((*iter)->handleCollision(m_bird))
+        {
+            m_loss = true;
+            break;
+        }
+    }
+    if(m_bird.getPosition().y < 0.0f || m_bird.getPosition().y >= 480.0f)
+    {
+        m_loss = true;
+        m_bird.stop();
+    }
+}
+
+void Game::reset()
+{
+    m_pipes.clear();
+    m_loss  = false;
+    m_ready = false;
+    m_bird.reset();
 }
